d1dc12db54181fa6ed3fa081698d99dda4d9324f
張欽揚 <crazyms520@hotmail.com>
2019-07-06 15:12:13 +0800

    Ex3

---
+++ ex3.rb
@@ -0,0 +1,34 @@
+# 計算有幾隻雞
+puts "I will now count my chickens:"
+# 母雞
+puts "Hens", 25 + 30 / 6.0
+# 公雞
+puts "Roosters", 100 - 25 * 3.0 % 4.0
+
+# 計算有幾顆蛋
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# 比大小
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+# 加法
+puts "What is 3 + 2?", 3 + 2
+# 減法
+puts "What is 5 - 7?", 5 - 7
+
+# 反省
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# 比大小是非題
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
+
+# BMI值計算公式: BMI = 體重(公斤) / 身高 ** 2(公尺 ** 2)
+puts "My BMI is?"
+puts 70 / (1.65 * 1.65)
