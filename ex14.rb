b27f54d2618fdfae8d0ccaae24cc29fcaac15a02
HankChang <bsuysheep927@gmail.com>
2019-07-08 22:02:53 +0800

    Ex14

---
+++ ex14.rb
@@ -0,0 +1,23 @@
+# 取得 ARGV 陣列 的第一個值賦予給 區域變數 user
+user = ARGV.first
+prompt = '>'
+
+puts "Hi #{user}, I'm the #{$0} script."
+puts "I'd like to ask you a few question."
+puts "Do you like me #{user}?"
+print prompt
+likes = STDIN.gets.chomp
+
+puts "Where do you live #{user}?"
+print prompt
+lives = STDIN.gets.chomp
+
+puts "What kind of computer do you have?"
+print prompt
+computer = STDIN.gets.chomp
+
+puts <<MESSAGE
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer} computer. Nice.
+MESSAGE
