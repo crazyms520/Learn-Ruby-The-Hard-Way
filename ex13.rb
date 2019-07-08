187cac2274a8930ae4a5d4a66ec8f1b8282d7202
HankChang <bsuysheep927@gmail.com>
2019-07-08 21:29:27 +0800

    Ex13.rb

---
+++ ex13.rb
@@ -0,0 +1,7 @@
+first, second, third = ARGV
+# ARGV 將接收到的參數存為陣列
+# #{$0} = 檔案名稱
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
